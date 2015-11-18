e527182251f4369585a75d8d6417556e43c5a334
Hronom <hronom@gmail.com>
2015-11-18 11:28:42 +0200

    Added the ability to run on standalone tomcat

--- src/main/java/com/github/hronom/test/spring/boot/security/SampleWebSecureApplication.java
+++ src/main/java/com/github/hronom/test/spring/boot/security/SampleWebSecureApplication.java
@@ -1,69 +1,24 @@
 package com.github.hronom.test.spring.boot.security;
 
 import com.github.hronom.test.spring.boot.security.configs.ApplicationSecurityConfig;
+import com.github.hronom.test.spring.boot.security.configs.EmbeddedServletContainerConfig;
 import com.github.hronom.test.spring.boot.security.configs.WebMvcConfig;
 
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 @EnableAutoConfiguration
 @ComponentScan
-@Import(value = {ApplicationSecurityConfig.class, WebMvcConfig.class})
-public class SampleWebSecureApplication extends WebMvcConfigurerAdapter {
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        tomcat.addAdditionalTomcatConnectors(createSslConnector());
-        return tomcat;
-    }
-
-    private Connector createSslConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-        try {
-            File keystore = getKeyStoreFile();
-            File truststore = keystore;
-            connector.setScheme("https");
-            connector.setSecure(true);
-            connector.setPort(8443);
-            protocol.setSSLEnabled(true);
-            protocol.setKeystoreFile(keystore.getAbsolutePath());
-            protocol.setKeystorePass("changeit");
-            protocol.setTruststoreFile(truststore.getAbsolutePath());
-            protocol.setTruststorePass("changeit");
-            protocol.setKeyAlias("apitester");
-            return connector;
-        } catch (IOException ex) {
-            throw new IllegalStateException(
-                "cant access keystore: [" + "keystore" + "] or truststore: [" + "keystore" + "]",
-                ex
-            );
-        }
-    }
-
-    private File getKeyStoreFile() throws IOException {
-        ClassPathResource resource = new ClassPathResource("keystore");
-        try {
-            return resource.getFile();
-        } catch (Exception ex) {
-            File temp = File.createTempFile("keystore", ".tmp");
-            FileCopyUtils.copy(resource.getInputStream(), new FileOutputStream(temp));
-            return temp;
-        }
+@Import(value = {
+    ApplicationSecurityConfig.class, WebMvcConfig.class, EmbeddedServletContainerConfig.class
+})
+public class SampleWebSecureApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleWebSecureApplication.class);
     }
 
     public static void main(String[] args) throws Exception {
