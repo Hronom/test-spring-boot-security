0734b19cf82a8659a4fefd27261851894af90a7d
Hronom <hronom@gmail.com>
2016-02-09 12:33:23 +0200

    Update Spring boot test 2

--- src/test/java/com/github/hronom/test/spring/boot/security/controllers/SampleControllerTestIT.java
+++ src/test/java/com/github/hronom/test/spring/boot/security/controllers/SampleControllerTestIT.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -17,20 +16,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.security.Principal;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // More info about annotations:
 // http://www.jayway.com/2014/07/04/integration-testing-a-spring-boot-application/
@@ -49,33 +40,6 @@ public class SampleControllerTestIT {
 
     @Autowired
     private EmbeddedWebApplicationContext server;
-
-    private static final class SessionHolder{
-        private SessionWrapper session;
-
-
-        public SessionWrapper getSession() {
-            return session;
-        }
-
-        public void setSession(SessionWrapper session) {
-            this.session = session;
-        }
-    }
-
-    private static class SessionWrapper extends MockHttpSession {
-        private final HttpSession httpSession;
-
-        public SessionWrapper(HttpSession httpSession){
-            this.httpSession = httpSession;
-        }
-
-        @Override
-        public Object getAttribute(String name) {
-            return this.httpSession.getAttribute(name);
-        }
-
-    }
 
     @Before
     public void setUp() {
