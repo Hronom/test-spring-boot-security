eeaa83d1efe0cbfd61cc208f36ee23b89d07f4ca
Andrew Podoprigora <andrew.podoprigora@gmail.com>
2016-02-09 11:52:30 +0200

    first IT test has created

---
+++ src/test/java/com/github/hronom/test/spring/boot/security/controllers/SampleControllerTestIT.java
@@ -0,0 +1,34 @@
+package com.github.hronom.test.spring.boot.security.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SampleControllerTestIT {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRoot() throws Exception {
+
+    }
+
+    @Test
+    public void testAdmin() throws Exception {
+
+    }
+
+    @Test
+    public void testUser() throws Exception {
+
+    }
+
+    @Test
+    public void testRoles() throws Exception {
+
+    }
+}
